1f8233acfa2005f92da3ec3abb647a724661edc5
193s <193sim@gmail.com>
2014-08-12 18:02:11 +0900

    */演算子を追加

--- Lang-K/src/Main.java
+++ Lang-K/src/Main.java
@@ -5,10 +5,10 @@ import lang.Extension;
 
 public class Main {
 	public static void main(String args[]) {
-		String s = "1+2-3";
-		Token[] ls = tokenizer(s);
-		AST ast = new Sum(ls, 0);
-		ast.eval(0);
+		String s = "(1+2)*3";
+		Token[] ls = tokenizer(s);	// 字句解析
+		AST ast = new Sum(ls, 0);	// 構文解析
+		System.out.println(ast.eval(0));
 	}
 
 	public static Token[] tokenizer(String s) {
@@ -20,6 +20,9 @@ public class Main {
 		}
 		return ret;
 	}
+
+	
+	
 	
 	// 抽象構文木
 	static class AST {
@@ -27,8 +30,12 @@ public class Main {
 		int num_token = 0;
 		// 構文木の構築に成功したかどうか
 		boolean ok = false;
-		void eval(int k) {}
+		int eval(int k) {
+			return 0;
+		}
 	}
+	
+	
 	
 	static class ASTList extends AST {
 		ArrayList<AST> children;
@@ -36,6 +43,7 @@ public class Main {
 			children = new ArrayList<AST>();
 		}
 	}
+	
 	
 	static class ASTLeaf extends AST {
 		Token child;
@@ -45,39 +53,10 @@ public class Main {
 			child = l;
 		}
 	}
+
 	
-	// Sum ::= Num {'+' Num | '-' Num } 
-	static class Sum extends ASTList {
-		Sum(Token[] ls, int offset) {
-			AST left = new Num(ls, offset);
-			if(!left.ok) return;
-			children.add(left);
-			num_token = left.num_token;
-			while (offset+num_token + 1 < ls.length) {
-				if (!(ls[offset + num_token] instanceof Token.Operator)) break;
-				if (!(((Token.Operator) ls[offset + num_token]).op.equals("+")) &&
-				   !(((Token.Operator) ls[offset + num_token]).op.equals("-"))) break;
-				AST right = new Num(ls, offset + num_token + 1);
-				if(!right.ok) break;
-				children.add(new ASTLeaf(ls[offset + num_token]));
-				children.add(right);
-				num_token += right.num_token + 1;
-			}
-			ok = true;
-		}
-		@Override
-		void eval(int k) {
-			for(int i=0; i<k; i++) System.out.print(" ");
-			System.out.println("sum");
-			children.get(0).eval(k+1);
-			for(int i=0; i<children.size(); i+=2) {
-				for(int j=0; j<k; j++) {
-					System.out.println(((ASTLeaf)children.get(i)).child.toString());
-					children.get(i+1).eval(k+1);
-				}
-			}
-		}
-	}
+	
+	// Num ::= '('Sum')' | NumberToken
 	static class Num extends ASTList {
 		Num(Token[] ls, int offset) {
 			if (ls[offset] instanceof Token.Operator) {
@@ -91,17 +70,113 @@ public class Main {
 				children.add(s);
 				ok = true;
 			}
+
 			else if(ls[offset] instanceof Token.Num) {
 				children.add(new ASTLeaf(ls[offset]));
 				num_token = 1;
 				ok = true;
 			}
 		}
-		
-		void eval(int k) {
+		@Override
+		int eval(int k) {
 			for(int i=0; i<k; i++) System.out.print(" ");
 			System.out.println("Num");
-			children.get(0).eval(k + 1);
+			AST firstChild = children.get(0);
+			return firstChild instanceof Sum ?
+					firstChild.eval(k + 1):
+					((Token.Num)((ASTLeaf)firstChild).child).num;
+		}
+	}
+	
+	
+	
+	// Sum ::= Prod { [+-] Prod }
+	static class Sum extends ASTList {
+		Sum(Token[] ls, int offset) {
+			AST left = new Prod(ls, offset);
+			if (!left.ok) return;
+			children.add(left);
+			num_token = left.num_token;
+			
+			while (offset + num_token + 1 < ls.length) {
+				Token nextToken = ls[offset + num_token];
+				if (!(nextToken instanceof Token.Operator)) break;
+				
+				if (!(((Token.Operator) nextToken).op.equals("+")) &&
+					!(((Token.Operator) nextToken).op.equals("-"))) break;
+				
+				AST right = new Prod(ls, offset + num_token + 1);
+				if(!right.ok) break;
+				children.add(new ASTLeaf(nextToken));
+				children.add(right);
+				
+				num_token += right.num_token + 1;
+			}
+			ok = true;
+		}
+		
+		@Override
+		int eval(int k) {
+			for(int i=0; i<k; i++) System.out.print(' ');
+			System.out.println("sum");
+			int ret = children.get(0).eval(k + 1);
+			for(int i=1; i<children.size(); i+=2) {
+				for(int j=0; j<k; j++) System.out.print(' ');
+				System.out.println( ((ASTLeaf)children.get(i)).child.toString() );
+				
+				int right = children.get(i + 1).eval(k + 1);
+				Token.Operator operator = (Token.Operator) ((ASTLeaf)children.get(i)).child;
+				if		(operator.op.equals("+")) ret += right;
+				else if (operator.op.equals("-")) ret -= right;
+			}
+			return ret;
+		}
+	}
+	
+	
+	// Prod ::= Num { [*/] Num }
+	static class Prod extends ASTList {
+		Prod(Token[] ls, int offset) {
+			AST left = new Num(ls, offset);
+			if (!left.ok) return;
+			children.add(left);
+			num_token = left.num_token;
+			
+			while (offset + num_token + 1 < ls.length) {
+				Token nextToken = ls[offset + num_token];
+				if (!(nextToken instanceof Token.Operator)) break;
+				
+				if (!(((Token.Operator)(nextToken)).op.equals("*")) &&
+					!(((Token.Operator)(nextToken)).op.equals("/"))) break;
+				
+				AST right = new Num(ls, offset + num_token + 1);
+				if (!right.ok) break;
+				children.add(new ASTLeaf(nextToken));
+				children.add(right);
+				
+				num_token += right.num_token + 1;
+			}
+			ok = true;
+		}
+		
+		@Override
+		int eval(int k) {
+			for(int i=0; i<k; i++) System.out.print(' ');
+			System.out.println("prod");
+			int ret = children.get(0).eval(k + 1);
+			
+			for(int i=1; i<children.size(); i+=2) {
+				for(int j=0; j<k; j++) System.out.print(' ');
+
+				System.out.println( ((ASTLeaf)children.get(i)).child.toString() );
+
+				int right = children.get(i + 1).eval(k + 1);
+
+				Token.Operator operator = (Token.Operator) (((ASTLeaf)children.get(i)).child);
+				if		(operator.op.equals("*")) ret *= right;
+				else if (operator.op.equals("/")) ret /= right;
+			}
+			return ret;
 		}
 	}
 }
