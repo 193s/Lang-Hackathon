c973c02f33819020bd706a9bf6e4c6563f370c8b
193s <193sim@gmail.com>
2014-08-15 21:59:57 +0900

    Token.javaとExtension.java上げ忘れてた

---
+++ Lang-K/src/Token.java
@@ -0,0 +1,37 @@
+
+public abstract class Token<T> {
+	protected Token(T value) {
+		this.value = value;
+	}
+	
+	private T value;
+	
+	public T getValue() {
+		return value;
+	}
+	
+	@Override
+	public String toString() {
+		return value + " : " + getClass().toString().substring(6);
+	}
+	
+	
+	
+	public static class Name extends Token<String> {
+		public Name(String value) {
+			super(value);
+		}
+	}
+	
+	public static class Num extends Token<Integer> {
+		public Num(int value) {
+			super(value);
+		}
+	}
+	
+	public static class Operator extends Token<String> {
+		public Operator(String value) {
+			super(value);
+		}
+	}
+}
---
+++ Lang-K/src/lang/Extension.java
@@ -0,0 +1,76 @@
+package lang;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/* this code has copied from processing.core.PApplet */
+public class Extension {
+	public static HashMap<String, Pattern> matchPatterns;
+
+	public static Pattern matchPattern(String regexp) {
+		Pattern p = null;
+		if (matchPatterns == null) {
+			matchPatterns = new HashMap<String, Pattern>();
+		} else {
+			p = matchPatterns.get(regexp);
+		}
+		if (p == null) {
+			if (matchPatterns.size() == 10) {
+				// Just clear out the match patterns here if more than 10 are
+				// being
+				// used. It's not terribly efficient, but changes that you have
+				// >10
+				// different match patterns are very slim, unless you're doing
+				// something really tricky (like custom match() methods), in
+				// which
+				// case match() won't be efficient anyway. (And you should just
+				// be
+				// using your own Java code.) The alternative is using a queue
+				// here,
+				// but that's a silly amount of work for negligible benefit.
+				matchPatterns.clear();
+			}
+			p = Pattern.compile(regexp, Pattern.MULTILINE | Pattern.DOTALL);
+			matchPatterns.put(regexp, p);
+		}
+		return p;
+	}
+
+	public static String[] match(String str, String regexp) {
+		Pattern p = matchPattern(regexp);
+		Matcher m = p.matcher(str);
+		if (m.find()) {
+			int count = m.groupCount() + 1;
+			String[] groups = new String[count];
+			for (int i = 0; i < count; i++) {
+				groups[i] = m.group(i);
+			}
+			return groups;
+		}
+		return null;
+	}
+
+	public static String[][] matchAll(String str, String regexp) {
+		Pattern p = matchPattern(regexp);
+		Matcher m = p.matcher(str);
+		ArrayList<String[]> results = new ArrayList<String[]>();
+		int count = m.groupCount() + 1;
+		while (m.find()) {
+			String[] groups = new String[count];
+			for (int i = 0; i < count; i++) {
+				groups[i] = m.group(i);
+			}
+			results.add(groups);
+		}
+		if (results.isEmpty()) {
+			return null;
+		}
+		String[][] matches = new String[results.size()][count];
+		for (int i = 0; i < matches.length; i++) {
+			matches[i] = results.get(i);
+		}
+		return matches;
+	}
+}
