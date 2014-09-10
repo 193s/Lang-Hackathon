f896a1bcd974ede48cce6ee5cfb2a358408385b5
193s <193sim@gmail.com>
2014-09-10 19:33:38 +0900

    つらい

---
+++ Lang-K_C#/myLang/myLang/Interpreter.cs
@@ -0,0 +1,64 @@
+﻿using System;
+//using System.Text.RegularExpressions;
+namespace MyLang
+{
+	public class Interpreter 
+	{
+		static void Main()
+		{
+			Console.WriteLine("Interpreter:");
+			string input, s = "";
+			do {
+				input = Console.ReadLine();
+				s += input + '\n';
+			}
+			while (input != "");
+
+			Token[] ls = Lexer.tokenize(s);
+			foreach (Token t in ls) Console.WriteLine("[{0}]\n", t);
+		}
+	}
+
+	public class Token
+	{
+		public Token(String s)
+		{
+			String = s;
+		}
+
+		public string String { get; protected set; }
+	}
+	public class ReservedToken : Token
+	{
+		public ReservedToken(string s) : base (s)
+		{}
+	}
+	public class IdentiferToken : Token
+	{
+		public IdentiferToken(string s) : base (s)
+		{}
+	}
+	public class OperatorToken : Token
+	{
+		public OperatorToken(string s) : base (s)
+		{}
+	}
+	public class LiteralToken : Token
+	{
+		public LiteralToken(string s) : base (s)
+		{
+			Value = int.Parse(s);
+		}
+		public int Value { get; private set; }
+	}
+
+	public class Lexer
+	{
+		public static Token[] tokenize(string s)
+		{
+
+			return null;
+		}
+	}
+}
+
