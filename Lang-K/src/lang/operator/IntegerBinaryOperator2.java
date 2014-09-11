aa4346b5104a5013ea81cfbb8bfb437d55df12b6
193s <193sim@gmail.com>
2014-09-11 11:30:54 +0900

    test

---
+++ Lang-K/src/lang/operator/IntegerBinaryOperator2.java
@@ -0,0 +1,34 @@
+package lang.operator;
+
+import java.util.HashMap;
+import java.util.function.BinaryOperator;
+
+public class IntegerBinaryOperator2 {
+	public static final HashMap<String, BinaryOp<Integer>> map;
+	
+	static {
+		map = new HashMap<String, BinaryOp<Integer>>() {
+			{
+//				map.put("+", new BinaryOp(1, (a,b) -> (Integer)a + (Integer)b));
+//				map.put("-", new BinaryOp<Integer>(1, (a,b) -> a - b));
+			}
+		};
+//		map.put("*", (a,b) -> a * b);
+//		map.put("/", (a,b) -> a / b);
+//		map.put("%", (a,b) -> a % b);
+	}
+	
+	public class BinaryOp<T> implements BinaryOperator<T> {
+		public int level;
+		private BinaryOperator<T> eval;
+		private BinaryOp(int level, BinaryOperator<T> eval) {
+			this.level = level;
+			this.eval = eval;
+		}
+
+		@Override
+		public T apply(T t, T u) {
+			return eval.apply(t, u);
+		}
+	}
+}
