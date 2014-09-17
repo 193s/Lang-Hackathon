a5800eb19e1843de9f4eff42f4a8d164e1e93dde
193s <193sim@gmail.com>
2014-09-17 17:35:43 +0900

    C#移植版の字句解析器を追加
    バグあり

--- Lang-K_C#/myLang/myLang/Interpreter.cs
+++ Lang-K_C#/myLang/myLang/Interpreter.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-//using System.Text.RegularExpressions;
-using LLVM;
+using System.Collections.Generic;
+
 namespace MyLang
 {
 	public class Interpreter 
@@ -15,51 +15,9 @@ namespace MyLang
 			}
 			while (input != "");
 
-			Token[] ls = Lexer.tokenize(s);
-			foreach (Token t in ls) Console.WriteLine("[{0}]\n", t);
-		}
-	}
-
-	public class Token
-	{
-		public Token(String s)
-		{
-			String = s;
-		}
-
-		public string String { get; protected set; }
-	}
-	public class ReservedToken : Token
-	{
-		public ReservedToken(string s) : base (s)
-		{}
-	}
-	public class IdentiferToken : Token
-	{
-		public IdentiferToken(string s) : base (s)
-		{}
-	}
-	public class OperatorToken : Token
-	{
-		public OperatorToken(string s) : base (s)
-		{}
-	}
-	public class LiteralToken : Token
-	{
-		public LiteralToken(string s) : base (s)
-		{
-			Value = int.Parse(s);
-		}
-		public int Value { get; private set; }
-	}
-
-	public class Lexer
-	{
-		public static Token[] tokenize(string s)
-		{
-
-			return null;
+			IEnumerable<Token> ls = Lexer.Tokenize(s);
+			Console.WriteLine ("---SUCCEED TOKENIZING---");
+			foreach (Token t in ls) Console.WriteLine("[{0}] : {1}", t.kind, t.String);
 		}
 	}
 }
-
