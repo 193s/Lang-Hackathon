db7b8ee8d131c7befdd62b756bf777e024eb39d0
193s <193sim@gmail.com>
2014-09-19 00:14:15 +0900

    いろいろ

---
+++ Lang-K/src/lang/parser/Environment.java
@@ -0,0 +1,10 @@
+package lang.parser;
+
+import java.util.HashMap;
+
+public class Environment {
+	public HashMap<String, Integer> hashMap;
+	public Environment() {
+		hashMap = new HashMap<String, Integer>();
+	}
+}
