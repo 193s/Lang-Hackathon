8d2a7c3f71b041dab5e94b9a876c628fa5f03c22
193s <193sim@gmail.com>
2014-09-19 22:47:39 +0900

    超cleanup

--- Lang-K/src/lang/lexer/Token.java
+++ Lang-K/src/lang/lexer/Token.java
@@ -1,40 +1,16 @@
 package lang.lexer;
 
-public abstract class Token {
-	public Token(String string) {
+public class Token {
+	public Token(String string, TokenKind kind) {
 		this.string = string;
+		this.kind = kind;
 	}
 	
 	public String string;
+	public TokenKind kind;
 	
 	@Override
 	public String toString() {
 		return string + "\t: " + getClass().getSimpleName();
-	}
-	
-	
-	public static class Reserved extends Token {
-		public Reserved(String value) {
-			super(value);
-		}
-	}
-	public static class Name extends Token {
-		public Name(String value) {
-			super(value);
-		}
-	}
-	
-	public static class Num extends Token {
-		public int num;
-		public Num(String value) {
-			super(value);
-			num = Integer.parseInt(value);
-		}
-	}
-	
-	public static class Operator extends Token {
-		public Operator(String value) {
-			super(value);
-		}
 	}
 }
