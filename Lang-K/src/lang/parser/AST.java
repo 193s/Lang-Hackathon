631cd43247e76fee73006abbc3d78e19b8d13eae
193s <193sim@gmail.com>
2014-09-20 00:26:50 +0900

    構成を変更

---
+++ Lang-K/src/lang/parser/AST.java
@@ -0,0 +1,10 @@
+package lang.parser;
+
+public class AST {
+	// 構文木の構築に成功したかどうか
+	boolean ok = false;
+	
+	public int eval(int k, Environment e) {
+		return 0;
+	}
+}
---
+++ Lang-K/src/lang/parser/Parser.java
@@ -0,0 +1,383 @@
+package lang.parser;
+
+import java.util.ArrayList;
+
+import lang.debug.Debug;
+import lang.lexer.Token;
+import lang.lexer.TokenSet;
+import lang.operator.BinaryOperatorIF;
+import lang.operator.IntegerBinaryOperator;
+
+public class Parser {
+	static public AST parse(TokenSet ls) {
+		AST ast = new Program(ls);
+		if (ast.ok) {
+			Debug.out.println("--SUCCEED PARSING--");
+		}
+		else {
+			Debug.out.println("ERROR: Parsing failed!");
+		}
+		return ast;
+	}
+}
+
+class ASTList extends AST {
+	ArrayList<AST> children;
+	ASTList() {
+		children = new ArrayList<AST>();
+	}
+}
+
+@Deprecated
+class ASTLeaf extends AST {
+	Token child;
+	ASTLeaf() {}
+	ASTLeaf(Token l) {
+		child = l;
+	}
+}
+
+
+class Variable extends AST {
+	String name;
+	Variable(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public int eval(int k, Environment e) {
+		Integer v = e.hashMap.get(name);
+		int value = (v == null)? 0: v;
+		Debug.log(k, "Variable : " + name + '(' + value + ')');
+		return value;
+	}
+}
+	
+class Literal extends AST {
+	int value;
+	Literal(TokenSet ls) {
+		Token t = ls.next();
+		value = Integer.parseInt(t.string);
+	}
+	Literal(Token token) {
+		value = Integer.parseInt(token.string);
+	}
+	Literal(String str) {
+		value = Integer.parseInt(str);
+	}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "Literal");
+		return value;
+	}
+}
+
+class Expr extends ASTList {
+	ArrayList<BinaryOperatorIF> operators;
+	
+	Expr(TokenSet ls) {
+		operators = new ArrayList<BinaryOperatorIF>();
+		Num n = new Num(ls);
+		if (!n.ok) return;
+		children = new ArrayList<AST>();
+		children.add(n);
+		
+		while (ls.isOperator()) {
+			String opstr = ls.readOperator().string;
+			
+			BinaryOperatorIF binaryOperator = null;
+			for (BinaryOperatorIF b: IntegerBinaryOperator.values()) {
+				if (b.getSign().equals(opstr)) {
+					binaryOperator = b;
+					break;
+				}
+			}
+			if (binaryOperator == null) {
+				ls.unget();
+				break;
+			}
+			operators.add(binaryOperator);
+			
+			Num n2 = new Num(ls);
+			if (!n2.ok) return;
+			children.add(n2);
+		}
+		ok = true;
+	}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "Expr");
+		ArrayList<Object> vals = new ArrayList<Object>();
+		
+		int count = 0;
+		for (AST v: children) {
+			vals.add(((Num)v).eval(k+1, e));
+			if (count >= operators.size()) break;
+			Debug.out.println(operators.get(count).getSign());
+			count++;
+		}
+		
+		ArrayList<BinaryOperatorIF> ops_cpy = new ArrayList<BinaryOperatorIF>(operators);
+		ArrayList<BinaryOperatorIF> ops_cpy2 = new ArrayList<BinaryOperatorIF>();
+		ArrayList<Object> vals_ = new ArrayList<Object>();
+		
+		int max_level = 5;
+		for (int il=0; il<max_level; il++) {
+			ops_cpy2.clear();
+			vals_.clear();
+			
+			vals_.add(vals.get(0));
+			for (int i=0; i<ops_cpy.size(); i++) {
+				if (ops_cpy.get(i).getLevel() == il) {
+					vals_.add(ops_cpy.get(i).eval(vals_.remove(vals_.size()-1), vals.get(i+1)));
+				} else {
+					vals_.add(vals.get(i+1));
+					ops_cpy2.add(ops_cpy.get(i));
+				}
+			}
+			ops_cpy = new ArrayList<BinaryOperatorIF>(ops_cpy2);
+			vals = new ArrayList<Object>(vals_);
+		}
+		return (Integer) vals.get(0);
+	}
+}
+
+class Num extends ASTList {
+	Num(TokenSet ls) {
+		Debug.out.println("num");
+		// ( Expression )
+		if (ls.isOperator()) {
+			Debug.out.println("( expression )");
+			if (!ls.read("(")) return;
+			AST s = new Expr(ls);
+			if (!s.ok) return;
+			if (!ls.read(")")) return;
+			children.add(s);
+		}
+
+		// Literal
+		else if (ls.isNumber()) {
+			Debug.out.println("literal");
+			children.add(new Literal(ls));
+		}
+		
+		// Variable
+		else if (ls.isName()) {
+			Debug.out.println("variable");
+			String ident = ls.next().string;
+			Variable v = new Variable(ident);
+			children.add(v);
+		}
+		else return;
+		
+		ok = true;
+	}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "Num");
+		AST child = children.get(0);
+		return child.eval(k+1, e);
+	}
+}
+
+
+class Statement extends ASTList {
+	Statement(TokenSet ls) {
+		AST child = 
+		  ls.isReserved("while")?	new While(ls)
+		: ls.isReserved("if")?		new If(ls)
+		: ls.isName()?				new Assign(ls)
+		: 							new Expr(ls)
+		;
+		children.add(child);
+		ok = true;
+	}
+
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "Statement");
+		AST child = children.get(0);
+		return child.eval(k+1, e);
+	}
+}
+
+
+class Program extends ASTList {
+	Program(TokenSet ls) {
+		AST s = new Statement(ls);
+		if (!s.ok) return;
+		children.add(s);
+
+		while (true) {
+			if (ls.isEOF()) break;
+			if (!ls.isMatch(",")) break;
+			Token operator = ls.next();
+			
+			AST right = new Statement(ls);
+			if (!right.ok) continue;
+
+			children.add(new ASTLeaf(operator));
+			children.add(right);
+		}
+		ok = true;
+	}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "Program");
+		int ret = children.get(0).eval(k+1, e);
+		
+		for (int i=1; i<children.size(); i++) {
+			if (children.get(i) instanceof Statement) ret = children.get(i).eval(k+1, e);
+			else {
+				Debug.log(k, ((ASTLeaf)children.get(i)).child.string);
+			}
+		}
+		return ret;
+	}
+}
+
+
+class Assign extends ASTList {
+	Assign(TokenSet ls) {
+		Debug.out.println("assign");
+		if (ls.isEOF()) return;
+		AST left = new Variable(ls.next().string);
+		
+		if (ls.isEOF()) return;
+		Token operator = ls.next();
+		if (!"=".equals(operator.string)) return;
+		
+		AST right = new Statement(ls);
+		if (!right.ok) return;
+		
+		children.add(left);
+		children.add(new ASTLeaf(operator));
+		children.add(right);
+
+		ok = true;
+	}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "Assign");
+		int ret = ((Statement)children.get(2)).eval(k+1, e);
+		// 代入
+		String identifier = ((Variable)children.get(0)).name;
+		e.hashMap.put(identifier, ret);
+		return ret;
+	}
+}
+
+
+class Condition extends ASTList {
+	Condition(TokenSet ls) {
+		Debug.out.println("condition");
+		AST left = new Expr(ls);
+		if (!left.ok) return;
+		children.add(left);
+		
+		if (!ls.isMatch("==", ">", "<")) return;
+		children.add(new ASTLeaf(ls.next()));
+		AST right = new Expr(ls);
+		if (!right.ok) return;
+		children.add(right);
+		
+		ok = true;
+	}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		String op = ((ASTLeaf) children.get(1)).child.string;
+		Debug.log(k, "Condition" + op);
+		
+		int left = children.get(0).eval(k+1, e);
+		int right = children.get(2).eval(k+1, e);
+		
+		int ret = 0;
+		if		(">".equals(op))  ret = left > right ? 1:0;
+		else if ("==".equals(op)) ret = left == right? 1:0;
+		else if ("<".equals(op))  ret = left < right ? 1:0;
+		
+		return ret;
+	}
+}
+
+
+class While extends ASTList {
+	While(TokenSet ls) {
+		Debug.out.println("while");
+		if (!ls.read("while", "(")) return;
+		AST condition = new Condition(ls);
+		if (!condition.ok) return;
+		if (!ls.read(")")) return;
+		AST block = new Block(ls);
+
+		children.add(condition);
+		children.add(block);
+		ok = true;
+	}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "While");
+		
+		AST condition = children.get(0);
+		AST program = children.get(1);
+		
+		int ret = 0;
+		while (condition.eval(k+1, e) == 1) {
+			ret = program.eval(k+1, e);
+		}
+		return ret;
+	}
+}
+class Block extends ASTList {
+	Block(TokenSet ls) {
+	Debug.out.println("block");
+		if (!ls.read(":")) return;
+		AST program = new Program(ls);
+		if (!ls.read(";")) return;
+		children.add(program);
+		ok = true;
+	}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "block");
+		return children.get(0).eval(k+1, e);
+	}
+}
+
+class If extends ASTList {
+	If(TokenSet ls) {
+		Debug.out.println("if");
+		if (!ls.read("if", "(")) return;
+		
+		AST condition = new Condition(ls);
+		if (!condition.ok) return;
+		if (!ls.read(")")) return;
+		AST block = new Block(ls);
+		
+		children.add(condition);
+		children.add(block);
+		ok = true;
+		}
+	
+	@Override
+	public int eval(int k, Environment e) {
+		Debug.log(k, "If");
+		
+		int ret = 0;
+		AST condition = children.get(0);
+		AST program = children.get(1);
+		
+		if (condition.eval(k+1, e) == 1) {
+			ret = program.eval(k+1, e);
+		}
+		return ret;
+	}
+}
