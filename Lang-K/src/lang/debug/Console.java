daa50cc492c5c3764f47c3a9c42312619b47b409
193s <193sim@gmail.com>
2014-09-25 15:36:11 +0900

    cleaned up

---
+++ Lang-K/src/lang/debug/Console.java
@@ -0,0 +1,7 @@
+package lang.debug;
+
+import java.io.PrintStream;
+
+public class Console {
+    public static PrintStream out = System.out;
+}
--- Lang-K/src/lang/parser/operator/IBinaryOperator.java
+++
@@ -1,7 +0,0 @@
-package lang.parser.operator;
-
-public interface IBinaryOperator<T> {
-	public T eval(T left, T right);
-	public int getLevel();
-	public String getSign();
-}
